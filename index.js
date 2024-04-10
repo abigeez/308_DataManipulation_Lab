d25461a5549800ed362df30f33359393fadfdeba
Abi Garner <abigail.gee@hotmail.com>
2024-04-10 00:53:09 -0400

    added two last answers

--- index.js
+++ index.js
@@ -47,7 +47,25 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
 //part one:
   // Checking if all numbers are divisible by 5 
 
-  const isDiv5 = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) === 0
+  const isDiv5 = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) === 0;
   console.log(isDiv5);
   //output is true, meaning that there are no remainders left from either of the values when they are divided by 5, 
   //resulting in all of them being divisible by 5.
+
+  //checking if the first number is greater than the last
+const largThan = n1 > n4;
+console.log(largThan)
+//output is true
+
+//artithmetic chain
+
+const subTract = (n2-n1);
+console.log(subTract);
+// output is 5, because 15-10 = 5
+
+const mulT = (subTract * n3);
+console.log(mulT);
+//output is 100 because 5 * 20 is 100.
+
+const remn4 = (mulT % n4);
+console.log(remn4);
